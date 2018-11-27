06ea0141a7b42d4abbe1964e5d97dbbf406316ae
polzin6874 <43218293+polzin6874@users.noreply.github.com>
2018-11-27 13:56:13 -0600

    Add files via upload

---
+++ madScript.js
@@ -0,0 +1,23 @@
+function madLibs ()
+{
+    var words = [];
+    words.push(document.madlibs.input1.value);
+    words.push(document.madlibs.input2.value);
+    words.push(document.madlibs.input3.value);
+    words.push(document.madlibs.input4.value);
+    words.push(document.madlibs.input5.value);
+    words.push(document.madlibs.input6.value);
+    words.push(document.madlibs.input7.value);
+    words.push(document.madlibs.input8.value);
+    words.push(document.madlibs.input9.value);
+    words.push(document.madlibs.input10.value);
+    words.push(document.madlibs.input11.value);
+    words.push(document.madlibs.input12.value);
+    words.push(document.madlibs.input13.value);
+    words.push(document.madlibs.input14.value);
+    words.push(document.madlibs.input15.value);
+    words.push(document.madlibs.input16.value);
+    words.push(document.madlibs.input17.value);
+    return "I love to " + words[0] + "video games. I can play them day and " + words[1] +"! My mom and " + words[2] + " are not too happy with my " + words[3] + "so much time in front of the television " + words[4]+ ". Although Dad believes that these"+ words[5] +"games help children develop hand "+ words[6] +" coordination and improve their learning "+ words[7] +", he also seems to think they have "+ words[8] +"side effects on one's "+ words[9] +". Both of my "+ words[10] +" think this is due to a "+ words[11] +" use of violence in the majority of the "+ words[12] +"Finally, we all arrived at a "+ words[13] +" compromise: After dinner I can play "+ words[14] +" hours of video games, provided I help clear the "+ words[15] +"and wash the "+ words[16] +"."
+
+}
